bb5d8b2565640afe014fbe14461eb43fadd0d5ce
Fadil Bantan <fadooly12@gmail.com>
2025-09-15 02:26:38 +0300

    History tracking for better conversations

--- main.py
+++ main.py
@@ -6,37 +6,42 @@ It loads the API key from environment variables, sends a prompt, and prints the 
 """
 
 import os, sys
-from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
+from dotenv import load_dotenv
 
 def main():
     """
     Loads the Gemini API key, sends a prompt to the Gemini model, and prints the response and token usage.
     """
     load_dotenv()  # Load environment variables from .env file
-
     api_key = os.environ.get("GEMINI_API_KEY")  # Retrieve Gemini API key
-
     client = genai.Client(api_key=api_key)  # Initialize Gemini client
 
-    # Check if a prompt is provided as a command-line argument
-    if len(sys.argv) > 1:
-        prompt = sys.argv[1]
-    else:
-        raise Exception("Please provide a prompt for the AI.")
+    args = sys.argv[1:]
+
+    if not args:
+        print("Please input a prompt like this:")
+        print("uv run main.py 'Insert Prompt Here!'")
+        print("Example: uv run main.py 'What is the meaning of life?'")
         exit(1)
 
-    # Generate content using Gemini model
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=prompt,
-    )
+    user_prompt = " ".join(args)
 
-    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+    )
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)  # Print prompt token count
+    print("Response tokens:", response.usage_metadata.candidates_token_count)  # Print response token count
     print(response.text)  # Print model response
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")  # Print prompt token count
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")  # Print response token count
 
 
 if __name__ == "__main__":
