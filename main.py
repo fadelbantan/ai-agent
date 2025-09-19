e6e6a0756a89b756c507f0ebef7bf4108a36695f
Fadil Bantan <fadooly12@gmail.com>
2025-09-19 16:41:25 +0300

    AI Agent can now get information on files, using get_file_info

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import os, sys, argparse
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+from functions.get_file_info import schema_get_files_info, available_functions
 
 def main():
     """
@@ -28,7 +29,7 @@ def main():
     args = parser.parse_args()
     user_prompt = args.prompt
 
-    if not args.prompt:
+    if not user_prompt:
         print("Please input a prompt like this:")
         print("uv run main.py 'Insert Prompt Here!'")
         print("Example: uv run main.py 'What is the meaning of life?'")
@@ -36,21 +37,36 @@ def main():
 
 
     messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),  
     ]
-
+    
+    
     generate_content(client, messages, user_prompt, args.verbose)
 
 def generate_content(client, messages, user_prompt, verbose):
+    system_prompt = """
+        You are a helpful AI coding agent.
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+        - List files and directories
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+        """
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+                tools=[available_functions], 
+                system_instruction=system_prompt
+        ),
     )
     if verbose:
-        print("User prompt:", user_prompt)  # Print model response
+        print("User prompt:", user_prompt)  # Print user's prompt
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)  # Print prompt token count
         print("Response tokens:", response.usage_metadata.candidates_token_count)  # Print response token count
-    print(response.text)
+    if response.function_calls and len(response.function_calls) > 0:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
